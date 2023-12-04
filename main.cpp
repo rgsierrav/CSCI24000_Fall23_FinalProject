023cec1bb49acdb7ad8aedcff1791e951d09f96e
Rosa G. Sierra Villanueva <rosagloriasv27@gmail.com>
2023-12-04 12:09:28 -0500

    update main and json

--- main.cpp
+++ main.cpp
@@ -1,12 +1,44 @@
 #include <iostream>
 #include <string>
 #include <limits>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include "nlohmann/json.hpp"
 #include "Cookbook.h"
 #include "Recipe.h"
 #include "Ingredient.h"
 
 using namespace std;
+using json = nlohmann::json;
 
+// Function to read JSON file
+std::string readJsonFile(const std::string& filename) {
+    std::ifstream file(filename);
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
+}
+
+// Function to parse JSON and add recipes to the cookbook
+void parseJson(Cookbook& cookbook, const std::string& jsonString) {
+    auto j = json::parse(jsonString);
+
+    for (const auto& item : j) {
+        std::string recipeName = item["Recipe Name"];
+        std::string ingredients = item["Ingredients"];
+        std::string directions = item["Directions"];
+
+        // Here, you need to convert the ingredients string to Ingredient objects
+        // For simplicity, let's assume you have a function that does this
+        std::vector<Ingredient> ingredientList = convertToIngredients(ingredients);
+
+        Recipe recipe(recipeName, ingredientList, directions);
+        cookbook.addRecipe(recipe);
+    }
+}
+
+// Function to display the menu
 void displayMenu() {
     cout << "\nDigital Cookbook" << endl;
     cout << "1. Add Recipe" << endl;
@@ -16,6 +48,7 @@ void displayMenu() {
     cout << "Enter your choice: ";
 }
 
+// Function to clear the input buffer
 void clearCin() {
     cin.clear();
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
@@ -23,6 +56,10 @@ void clearCin() {
 
 int main() {
     Cookbook myCookbook;
+    string jsonFile = "recipes.json"; // Path to your JSON file
+    string jsonString = readJsonFile(jsonFile);
+    parseJson(myCookbook, jsonString);
+
     int choice;
 
     while (true) {
@@ -35,93 +72,8 @@ int main() {
             continue;
         }
 
-        switch (choice) {
-            case 1: {
-                cout << "Enter recipe name: ";
-                string recipeName;
-                clearCin();
-                getline(cin, recipeName);
-
-                vector<Ingredient> ingredients;
-                cout << "How many ingredients? ";
-                int numIngredients;
-                cin >> numIngredients;
-
-                if (cin.fail()) {
-                    clearCin();
-                    cout << "Invalid input. Please enter a number." << endl;
-                    break;
-                }
-
-                clearCin();
-                for (int i = 0; i < numIngredients; ++i) {
-                    cout << "Enter ingredient name: ";
-                    string ingredientName;
-                    getline(cin, ingredientName);
-
-                    cout << "Enter quantity: ";
-                    double quantity;
-                    cin >> quantity;
-
-                    if (cin.fail()) {
-                        clearCin();
-                        cout << "Invalid input. Please enter a number." << endl;
-                        break;
-                    }
-
-                    clearCin();
-                    ingredients.push_back(Ingredient(ingredientName, quantity));
-                }
-
-                string steps;
-                cout << "Enter the preparation steps: ";
-                getline(cin, steps);
-
-                Recipe newRecipe(recipeName, ingredients, steps);
-                myCookbook.addRecipe(newRecipe);
-                cout << "Recipe added successfully." << endl;
-                break;
-            }
-            case 2: {
-                cout << "Enter recipe name to view: ";
-                string recipeName;
-                clearCin();
-                getline(cin, recipeName);
-
-                try {
-                    Recipe recipe = myCookbook.getRecipe(recipeName);
-                    recipe.displayRecipe();
-                } catch (const std::exception& e) {
-                    cout << "Recipe not found." << endl;
-                }
-                break;
-            }
-            case 3: {
-                cout << "Enter search query: ";
-                string searchQuery;
-                clearCin();
-                getline(cin, searchQuery);
-
-                vector<Recipe> foundRecipes = myCookbook.searchRecipes(searchQuery);
-
-                if (foundRecipes.empty()) {
-                    cout << "No recipes found matching your query." << endl;
-                } else {
-                    cout << "Found Recipes:" << endl;
-                    for (const auto& recipe : foundRecipes) {
-                        cout << recipe.getName() << endl;
-                    }
-                }
-                break;
-            }
-            case 4: {
-                cout << "Exiting Digital Cookbook." << endl;
-                return 0;
-            }
-            default: {
-                cout << "Invalid choice. Please try again." << endl;
-            }
-        }
+        // Implement the rest of your menu functionality here
+        // ...
     }
 
     return 0;
